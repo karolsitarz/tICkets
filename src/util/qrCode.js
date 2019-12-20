65d8af5856115681ab320dfb2baa051134a0471e
Karol Sitarz <ka.sitarz@gmail.com>
2019-12-20 02:30:55 +0100

    change code render method to canvas

--- src/util/qrCode.js
+++ src/util/qrCode.js
@@ -2,31 +2,35 @@ const RGB = 3;
 const pxSize = 2;
 const scale = 4;
 
-export default data => {
+const getQrSize = data => Math.sqrt(data.length / RGB) / pxSize;
+
+const getCodeArray = data => {
   const temp = [];
-  const qrSize = Math.sqrt(data.length / RGB) / pxSize;
+  const qrSize = getQrSize(data);
 
   for (let i = 0; i < qrSize; i++)
     for (let j = 0; j < qrSize; j++)
       temp[i * qrSize + j] = data[(i * qrSize * pxSize + j) * RGB * pxSize];
 
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttribute('viewBox', `0 0 ${qrSize * scale} ${qrSize * scale}`);
-  // svg.style.maxWidth = '500px';
-
-  temp.forEach((el, i) => {
-    if (el < 250) {
-      const point = document.createElementNS(
-        'http://www.w3.org/2000/svg',
-        'rect'
-      );
-      point.setAttribute('width', scale);
-      point.setAttribute('height', scale);
-      point.setAttribute('x', (i % qrSize) * scale);
-      point.setAttribute('y', Math.floor(i / qrSize) * scale);
-      // point.style.shapeRendering = 'crispEdges';
-      svg.appendChild(point);
-    }
-  });
-  return svg;
+  return temp;
 };
+
+const getCanvas = data => {
+  const code = getCodeArray(data);
+  const qrSize = getQrSize(data);
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = qrSize * scale;
+  canvas.height = qrSize * scale;
+  ctx.fillStyle = '#000000';
+
+  code.forEach((el, i) => {
+    const x = (i % qrSize) * scale;
+    const y = Math.floor(i / qrSize) * scale;
+    if (el < 250) ctx.fillRect(x, y, scale, scale);
+  });
+  return canvas;
+};
+
+export default getCanvas;
